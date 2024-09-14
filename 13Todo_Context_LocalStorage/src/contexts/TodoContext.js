d2f06fe96e8385ecb520906c6f60d922a5d1861f
shravankumarpatell <shravankumarpatelofficial@gmail.com>
2024-09-14 18:28:25 +0530

    completed Todo

---
+++ 13Todo_Context_LocalStorage/src/contexts/TodoContext.js
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        { id: 1, todo: "Todo Msg", completed: false },
+    ],
+    addTodo: (text) => {},
+    toggleComplete: (id) => {},
+    deleteTodo: (id) => {},
+    updatedTodo: (id, todo) => {}
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider;
